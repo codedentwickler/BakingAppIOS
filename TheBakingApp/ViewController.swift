1ae4a38caa2bbcbaf7434992464267fab431392c
Kanyinsola Fapohunda <kanyinsola.fapohunda@cottacush.com>
2018-01-24 11:15:37 +0100

    Initial Commit

---
+++ TheBakingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheBakingApp
+//
+//  Created by codedentwickler on 1/24/18.
+//  Copyright © 2018 codedentwickler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
